eb8e9333bee78c4904240c72051ca5cfd9c7dcdd
karynella <karynella@live.com>
2022-01-18 20:30:01 +0000

    added footer
    added an open source code and personal website footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <p>Hello everyone</p>
+      Coded by <a href="http://karyn.fun">Karynella</a> and is
+      <footer>
+        <a
+          href="https://github.com/karynella/final-weather-react-app"
+          target="blank"
+        >
+          Open source code
+        </a>
+      </footer>
     </div>
   );
 }
