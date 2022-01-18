98d78f8447db9befd120c3ec1e3d405568fb05b4
karynella <karynella@live.com>
2022-01-18 20:43:23 +0000

    added bootstrap
    added bootstrap and container to app.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Hello everyone</p>
-      Coded by <a href="http://karyn.fun">Karynella</a> and is
-      <footer>
-        <a
-          href="https://github.com/karynella/final-weather-react-app"
-          target="blank"
-        >
-          Open source code
-        </a>
-      </footer>
+      <div className="container">
+        <p>Hello everyone</p>
+        Coded by <a href="http://karyn.fun">Karynella</a> and is
+        <footer>
+          <a
+            href="https://github.com/karynella/final-weather-react-app"
+            target="blank"
+          >
+            Open source code
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
