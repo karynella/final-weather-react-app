b8408f52ded6fd65405d1210c0561d7bdc9af441
karynella <karynella@live.com>
2022-01-20 19:34:15 +0000

    updates
    added API integration
    installed axios

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Poole" />
 
         <footer>
           Coded by <a href="http://www.karyn.fun">Karynella</a> and is{" "}
