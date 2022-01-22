81d16f577559665f7bacdcd6a66523a95ee2e8a4
karynella <karynella@live.com>
2022-01-22 16:09:41 +0000

    changed css

--- src/Weather.js
+++ src/Weather.js
@@ -53,7 +53,7 @@ export default function Weather(props) {
             <div className="col-3">
               <input
                 type="submit"
-                value="search"
+                value="Search"
                 className="btn btn-primary w-100"
               />
             </div>
